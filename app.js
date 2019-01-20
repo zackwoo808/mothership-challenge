97c9d8478e9a3033f5e5fff3c1f8408f0e6329de
Zack Woo <zwoo18@bu.edu>
2019-01-20 13:02:42 -0800

    full functionality for one shipment. need to concurrently send axios requests and scale to all shipments

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-const bodyParser = require('body-parser');
-const express    = require('express');
-const morgan     = require('morgan');
-const PORT       = process.env.PORT || 3000;
-
-const app = express();
-
 // data
 const drivers = require('./data/drivers.json');
 const shipments = require('./data/shipments.json');
-
-// methods
-let degreesToRadians = (degrees) => {
-    return degrees * (Math.PI / 180);
-};
-
-let haversineFormula = (shipment, driver) => {
-    const R = 6371000; // meters
-    let lat1 = shipment.coordinates.latitude;
-    let lat2 = driver.coordinates.latitude;
-    let lon1 = shipment.coordinates.longitude;
-    let lon2 = driver.coordinates.longitude;
-    let Δlat = degreesToRadians(lat2 - lat1);
-    let Δlon = degreesToRadians(lon2 - lon1);
-
-    let a = Math.sin(Δlat / 2) * Math.sin(Δlat / 2) +
-        Math.cos(lat1) * Math.cos(lat2) *
-        Math.sin(Δlon / 2) * Math.sin(Δlon / 2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    let dist = R * c;
-
-    return dist;
-}
-
-let getProximity = (shipment, driver) => { // haversine formula
-    return haversineFormula(shipment, driver);
-};
+const shipmentIds = Object.keys(shipments);
+const numShipments = shipmentIds.length;
 
 // jobs
+const { offerShipment } = require('./jobs/offerShipmentJob.js');
+
+// console.log(shipments[shipmentIds[0]]);
+// console.log(drivers);
+
+offerShipment(shipments[shipmentIds[0]], drivers, shipmentIds[0]);
--- jobs/offerShipmentJob.js
+++ jobs/offerShipmentJob.js
@@ -1,5 +1,96 @@
-const { CronJob } = require('cron');
+const axios            = require('axios');
+const { CronJob }      = require('cron');
+const { getProximity } = require('../methods/get-proximity.js');
 
+let currentShipment = {};
+let shipmentId = 0;
+let driversList;
+let driverIdList;
+//console.log('Driver ID List: ' + driverIdList);
+let numDrivers;
+//console.log('Number of drivers: ' + numDrivers);
+let driverDistances = new Array(numDrivers);
+let numDenies = 0;
 
+/* --------------------------------------------------------------------------------- */
 
-module.exports = unreviewedPointsEmailJob;
+let sortedDriverDistances;
+//console.log('Driver Distances: ' + driverDistances);
+//console.log('Sorted Driver Drivers: ' + sortedDriverDistances);
+
+let currentDriver = 0; // testing
+//console.log('Current Driver Distance: ' + sortedDriverDistances[currentDriver]);
+let driverId;
+//console.log(driverId);
+
+/* --------------------------------------------------------------------------------- */
+
+const onComplete = () => {
+    console.log('Completed shipment');
+}
+
+const onTick = () => {
+    axios({
+        method: 'post',
+        url: `http://challenge.shipwithbolt.com/driver/${driverId}/dispatch`,
+        data: {
+            "shipmentId": Number(shipmentId),
+        }
+    }).then(res => {
+        console.log(`Package ${res.data.shipmentId} was ${res.data.response} by driver ${res.data.driverId}.`);
+        if (res.data.response === 'Accepted') {
+            offerShipmentJob.stop(); // stop when accepted
+        }
+        else if (res.data.response === 'Denied') {
+            driverId = driverDistances.indexOf(sortedDriverDistances[++currentDriver]) + 1;
+            numDenies++;
+        }
+    }).catch(error => { // error handling
+        console.log(error.response.status + ' ' + error.response.statusText);
+        console.log(error.response.data.message);
+        if (error.response.status === 400) {
+            console.log('Shipment ID used: ' + shipmentId);
+        }
+        else if (error.response.status === 404) {
+            console.log('Driver ID used: ' + driverId);
+        }
+    });
+
+    if (numDenies >= numDrivers) {
+        console.log('Driver not found. Please try again.');
+        offerShipmentJob.stop(); // stop when all drivers have denied... too hacky?
+    }
+}
+
+const offerShipmentJob = new CronJob({
+    cronTime: '*/10 * * * * *', // every 10 seconds
+    onComplete,
+    onTick,
+    start: false,
+    timeZone: 'America/Los_Angeles',
+});
+
+const offerShipment = (shipment, drivers, shipmentIdentification) => {
+    currentShipment = shipment;
+    driversList = drivers;
+    driverIdList = Object.keys(driversList);
+    shipmentId = shipmentIdentification;
+    numDrivers = driverIdList.length;
+
+    for (let i = 0; i < numDrivers; i++) {
+        driverDistances[i] = getProximity(currentShipment, driversList[i + 1]);
+    }
+
+    sortedDriverDistances = [...driverDistances];
+    sortedDriverDistances.sort((a, b) => (a - b));
+
+    driverId = driverDistances.indexOf(sortedDriverDistances[currentDriver]) + 1;
+
+    offerShipmentJob.start();
+}
+
+// offerShipmentJob.start(); // testing
+
+module.exports = {
+    offerShipment: offerShipment,
+}
